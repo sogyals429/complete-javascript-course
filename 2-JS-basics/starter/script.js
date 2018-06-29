bcbe807f3754a6158ffadf84a909a6a3ace4e80b
Sogyal.T.Sherpa <sogyals429@gmail.com>
2018-06-30 09:57:34 +1000

    Variables

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -1,1 +1,16 @@
 console.log("Hello World!");
+var firstName = 'Sogyal';
+var lastName = 'Sherpa';
+var age = 19;
+
+var fullAge = true;
+console.log(fullAge);
+console.log("Welcome " + firstName+" "+lastName + " your age is: " + age + " years.");
+
+var job;
+console.log(job);
+
+job = 'Teacher';
+console.log(job);
+
+var $53;
