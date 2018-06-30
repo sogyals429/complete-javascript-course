a582e08df2594f72c0fadfd10ecf5a12cf5810d0
Sogyal.T.Sherpa <sogyals429@gmail.com>
2018-06-30 12:02:07 +1000

    Type Coercion, Variable Mutation and Alerts

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -1,3 +1,5 @@
+/* Variables and Data Types*/
+/*
 console.log("Hello World!");
 var firstName = 'Sogyal';
 var lastName = 'Sherpa';
@@ -12,5 +14,29 @@ console.log(job);
 
 job = 'Teacher';
 console.log(job);
+*/
 
-var $53;
+/* Variable mutation and type coercion */
+
+var firstName = 'Sogyal';
+var age = 19;
+
+// Type Coercion
+console.log(firstName + " " + age);
+
+var job,isMarried;
+job = 'Student';
+isMarried = false;
+
+console.log(firstName + " is a " + age + " year old " + job + " and is he married?" + isMarried);
+
+//Variable Mutation
+
+age = 'twenty eight';
+job = 'driver';
+
+alert(firstName + " is a " + job);
+
+var lastName = prompt("What is his last name: ");
+console.log(lastName);
+
