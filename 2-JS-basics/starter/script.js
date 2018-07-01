cc84d580be491e58021f891170adaa81c75ff598
Sogyal.T.Sherpa <sogyals429@gmail.com>
2018-07-01 20:02:14 +1000

    Boolean Logic

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -139,3 +139,16 @@ if(isMarried){
     console.log(firstName + " is married.");
 }
 */
+
+var firstName = "Sogyal";
+var age = 19;
+
+if(age<13){
+    console.log(firstName + " is a boy.");
+}else if(age>=13 && age<20){
+    console.log(firstName + " is a teenager.");
+}else if(age>=20 && age<30){
+    console.log(firstName + " is a young man.");
+}else{
+    console.log(firstName + " is a man.");
+}
