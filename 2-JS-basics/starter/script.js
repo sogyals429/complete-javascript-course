682ece5cc6f09a8c049be355e922bcaebdd6c639
Sogyal.T.Sherpa <sogyals429@gmail.com>
2018-07-03 16:55:13 +1000

    Coding Challenge 5

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -479,3 +479,34 @@ for(var i=names.length-1;i>=0;i--){
 //     console.log(names[i]);
 // }
 */
+
+/* Coding Challenge 5 
+
+var sogyal = {
+    fullName : "Sogyal Sherpa",
+    bills: [124,48,268,180,42],
+    calcTips: function(){
+        this.tips = [];
+        this.finalValues = [];
+        
+        for(var i=0;i<this.bills.length;i++){
+            var percentage;
+            var bill = this.bills[i];
+
+                if(bill < 50){
+                    percentage = .2;
+                }else if(bill>=50 && bill<200){
+                    percentage = .15;
+                }else{
+                    percentage = .1;
+                }
+
+                this.tips[i] = bill*percentage;
+                this.finalValues[i] = bill+bill * percentage;
+        }
+    }
+}
+
+sogyal.calcTips();
+console.log(sogyal);                    
+*/
